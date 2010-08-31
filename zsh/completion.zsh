7c79cd8e8c0d032af9cae96d90512d18e7483b4f
Kalvir Sandhu <kalv@kalv.co.uk>
2010-08-31 16:10:16 +0100

    use existing rake task completion naming

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -9,10 +9,10 @@ zstyle ':completion:*' insert-tab pending
 
 # Handle rake tasks.
 _rake_does_task_list_need_generating () {
-if [ ! -f .rake_tasks ]; then
+if [ ! -f .rake_tasks~ ]; then
   return 0;
 else
-  accurate=$(stat -f%m .rake_tasks)
+  accurate=$(stat -f%m .rake_tasks~)
   changed=$(stat -f%m Rakefile)
   return $(expr $accurate '>=' $changed)
 fi
@@ -21,9 +21,9 @@ _rake () {
 if [ -f Rakefile ]; then
   if _rake_does_task_list_need_generating; then
     #echo "\nGenerating .rake_tasks..." > /dev/stderr
-    rake --silent --tasks | cut -d " " -f 2 > .rake_tasks
+    rake --silent --tasks | cut -d " " -f 2 > .rake_tasks~
   fi
-  compadd `cat .rake_tasks`
+  compadd `cat .rake_tasks~`
 fi
 }
 compdef _rake rake
