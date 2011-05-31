66c4e370f2ff576bb5a4da359602f7f2c45b4539
Kalvir Sandhu <kalv@kalv.co.uk>
2011-05-31 11:42:49 +0100

    Remove some things not being used anymore

--- zsh/osx/osx.zsh
+++ zsh/osx/osx.zsh
@@ -1,6 +1,6 @@
 # OS X specific settings
 
-export PATH=/usr/local/share/npm/bin:~/.homebrew/bin:~/usr/local/bin:/opt/local/bin:/opt/local/sbin:/usr/local/mysql/bin:~/Development/android-sdk-mac_86:$PATH
+export PATH=~/usr/local/bin:/usr/local/sbin:$PATH
 export SVN_EDITOR='mate -w'
 export GIT_EDITOR='mate -wl1'
 export EDITOR='mate'
@@ -8,20 +8,14 @@ export GEM_OPEN_EDITOR='mate'
 export LESSEDIT='mate -l %lm %f'
 export COMMAND_MODE=legacy
 
+export NODE_PATH=/usr/local/lib/node
+
 # ree
 export RUBY_HEAP_MIN_SLOTS=800000
 export RUBY_HEAP_FREE_MIN=100000
 export RUBY_HEAP_SLOTS_INCREMENT=300000
 export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
 export RUBY_GC_MALLOC_LIMIT=79000000
-
-# EC2
-export JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Home"
-export EC2_PRIVATE_KEY="$(/bin/ls $HOME/.ec2/pk-*.pem)"
-export EC2_CERT="$(/bin/ls $HOME/.ec2/cert-*.pem)"
-export EC2_AMITOOL_HOME="/usr/local/Cellar/ec2-ami-tools/1.3-45758/jars"
-export EC2_HOME="/usr/local/Cellar/ec2-api-tools/1.3-53907/jars"
-export EC2_URL="https://eu-west-1.ec2.amazonaws.com/"
 
 # Use OS X version of SSH with agent forwarding
 alias ssh='/usr/bin/ssh -A'
@@ -32,11 +26,6 @@ alias ls='ls -ahGl'
 
 fpath=(~/.dotfiles/zsh/osx/functions $fpath)
 autoload -U ~/.dotfiles/zsh/osx/functions/*(:t)
-
-localhost() {
-  sudo dscl localhost -create /Local/Default/Hosts/$1 IPAddress 127.0.0.1
-  echo "Added $1 at address 127.0.0.1"
-}
 
 case $OSTYPE in
   darwin10*)
