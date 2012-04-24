ab6b5773e1ae3645a496bf4948181d723a8f3175
Kalvir Sandhu <kalv@kalv.co.uk>
2012-04-23 19:03:03 -0700

    Updating environment to handle ec2 tools and change of editor to sublime text 2

--- zsh/osx/osx.zsh
+++ zsh/osx/osx.zsh
@@ -1,11 +1,9 @@
 # OS X specific settings
 
 export PATH=/Users/`whoami`/.dotfiles/bin:/usr/local/bin:~/usr/local/bin:/usr/local/sbin:/users/kalv/Development/android-sdk-mac_x86/tools:/users/kalv/Development/android-sdk-mac_x86/platform-tools:$PATH
-export SVN_EDITOR='mate -w'
-export GIT_EDITOR='mate -wl1'
-export EDITOR='mate'
-export GEM_OPEN_EDITOR='mate'
-export LESSEDIT='mate -l %lm %f'
+export GIT_EDITOR='subl'
+export EDITOR='subl'
+export GEM_OPEN_EDITOR='subl'
 export COMMAND_MODE=legacy
 
 export NODE_PATH=/usr/local/lib/node
@@ -16,6 +14,13 @@ export RUBY_HEAP_FREE_MIN=100000
 export RUBY_HEAP_SLOTS_INCREMENT=300000
 export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
 export RUBY_GC_MALLOC_LIMIT=79000000
+
+# EC2 keys
+export JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Home"
+export EC2_PRIVATE_KEY="$(/bin/ls $HOME/.ec2/pk-*.pem)"
+export EC2_CERT="$(/bin/ls $HOME/.ec2/cert-*.pem)"
+export EC2_AMITOOL_HOME="/usr/local/Cellar/ec2-ami-tools/1.3-45758/jars"
+export EC2_HOME="/usr/local/Cellar/ec2-api-tools/1.4.4.1/jars"
 
 # Use OS X version of SSH with agent forwarding
 alias ssh='/usr/bin/ssh -A'
