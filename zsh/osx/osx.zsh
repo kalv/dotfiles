a0afe718fb28dd9b7f684b494751fce8e0ac167d
Kalvir Sandhu <kalv@kalv.co.uk>
2012-10-24 12:35:28 -0700

    Don't need to set up ec2 and ree stuff anymore

--- zsh/osx/osx.zsh
+++ zsh/osx/osx.zsh
@@ -8,19 +8,12 @@ export COMMAND_MODE=legacy
 
 export NODE_PATH=/usr/local/lib/node:/usr/local/lib/node_modules
 
-# ree
-export RUBY_HEAP_MIN_SLOTS=800000
-export RUBY_HEAP_FREE_MIN=100000
-export RUBY_HEAP_SLOTS_INCREMENT=300000
-export RUBY_HEAP_SLOTS_GROWTH_FACTOR=1
-export RUBY_GC_MALLOC_LIMIT=79000000
-
 # EC2 keys
 export JAVA_HOME="/System/Library/Frameworks/JavaVM.framework/Home"
-export EC2_PRIVATE_KEY="$(/bin/ls $HOME/.ec2/pk-*.pem)"
-export EC2_CERT="$(/bin/ls $HOME/.ec2/cert-*.pem)"
-export EC2_AMITOOL_HOME="/usr/local/Cellar/ec2-ami-tools/1.3-45758/jars"
-export EC2_HOME="/usr/local/Cellar/ec2-api-tools/1.4.4.1/jars"
+#export EC2_PRIVATE_KEY="$(/bin/ls $HOME/.ec2/pk-*.pem)"
+#export EC2_CERT="$(/bin/ls $HOME/.ec2/cert-*.pem)"
+#export EC2_AMITOOL_HOME="/usr/local/Cellar/ec2-ami-tools/1.3-45758/jars"
+#export EC2_HOME="/usr/local/Cellar/ec2-api-tools/1.4.4.1/jars"
 
 # Use OS X version of SSH with agent forwarding
 alias ssh='/usr/bin/ssh -A'
