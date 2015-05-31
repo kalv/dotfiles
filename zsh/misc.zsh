19f43bed99ce34dd793d2dfb8197cb9c66c7727a
Kalvir Sandhu <kalv@kalv.co.uk>
2015-05-31 15:20:16 -0700

    Setting up new dotfiles that use oh-my-zsh

--- zsh/misc.zsh
+++
@@ -1,9 +0,0 @@
-bindkey '^H' backward-delete-char
-bindkey "^[[3~" delete-char
-setopt autocd
-
-setopt NO_BG_NICE # don't nice background tasks
-setopt NO_HUP
-setopt NO_LIST_BEEP
-#setopt CORRECT
-#setopt COMPLETE_IN_WORD
