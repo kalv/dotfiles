cccb6c852ef6c2a9af7dcc9ba89e2ba61c064d95
Kalvir Sandhu <kalv@kalv.co.uk>
2015-07-03 18:00:21 -0700

    Cleanup and removal of osx specific defaults

--- zsh/base.zsh
+++ zsh/base.zsh
@@ -7,12 +7,12 @@ source ~/.dotfiles/zsh/misc.zsh
 source ~/.dotfiles/zsh/history.zsh
 source ~/.dotfiles/zsh/aliases.zsh
 
-autoload -U ~/.dotfiles/zsh/functions/*(:t)
+# autoload -U ~/.dotfiles/zsh/functions/*(:t)
 
 #setopt LOCAL_OPTIONS # allow functions to have local options
 #setopt LOCAL_TRAPS # allow functions to have local traps
 
-cdpath=(~/Development/clients ~/Development/apps ~/Development/javascript ~/Development/ruby ~/Development/caliper)
+cdpath=(~/Development/clients ~/Development/apps ~/Development/javascript ~/Development/ruby ~/Development/sites)
 
 #setopt autopushd # Use pushd for all directory changing
 
