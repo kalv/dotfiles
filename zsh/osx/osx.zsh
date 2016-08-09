c01c40c13bba0bbbef704c99d9cbc249214fb285
Kalvir Sandhu <kalv@kalv.co.uk>
2016-08-08 17:32:34 -0700

    Hub doesn't play well with atom and setting config for latest neovim

--- zsh/osx/osx.zsh
+++ zsh/osx/osx.zsh
@@ -1,8 +1,8 @@
 # OS X specific settings
 
 export PATH=/Users/`whoami`/.dotfiles/bin:/usr/local/bin:~/usr/local/bin:/usr/local/sbin:/usr/local/share/npm/bin:~/.rbenv/bin:$PATH
-export GIT_EDITOR='atom'
-export EDITOR='atom'
+export GIT_EDITOR='nvim'
+export EDITOR='nvim'
 export COMMAND_MODE=legacy
 
 export NODE_PATH=/usr/local/lib/node:/usr/local/lib/node_modules
@@ -17,11 +17,9 @@ esac
 
 ulimit -n 4096
 
-export PATH=./bin:$PATH
-
 # When using Go
 # export PATH=$PATH:/usr/local/opt/go/libexec/bin
 #
 # export GOPATH=/usr/local/opt/go/libexec
 
-export PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
+export PATH="./bin:$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
