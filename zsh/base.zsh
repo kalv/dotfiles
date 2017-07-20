bfe38a76f1d40ac1e52c309d1e5b2a1ed98464fb
Kalvir Sandhu <kalv@kalv.co.uk>
2017-07-20 10:03:15 -0700

    Further tweaks to linux setup

--- zsh/base.zsh
+++ zsh/base.zsh
@@ -5,7 +5,7 @@ source ~/.dotfiles/zsh/aliases.zsh
 cdpath=(~/Development/elixir ~/Development/clients ~/Development/apps ~/Development/javascript ~/Development/ruby ~/Development/sites)
 
 # Load vendor specific scripts
-source ~/.dotfiles/zsh/osx/osx.zsh
+# source ~/.dotfiles/zsh/osx/osx.zsh
 source ~/.dotfiles/zsh/completion.zsh
 
-source ~/.dotfiles/zsh/history.zsh
+#source ~/.dotfiles/zsh/history.zsh
