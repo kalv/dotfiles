53c455ca498e0ba17880289bd2e8552d59d722a5
Kalvir Sandhu <kalv@kalv.co.uk>
2017-11-13 14:35:19 -0800

    Updating some things

--- zsh/base.zsh
+++ zsh/base.zsh
@@ -12,3 +12,6 @@ source ~/.dotfiles/zsh/history.zsh
 
 # brew install zsh-syntax-highlighting
 source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+# git clone git://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
