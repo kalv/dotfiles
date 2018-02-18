4188fc1c78bca71f115f329ad575d9939194f6be
Kalvir Sandhu <kalv@kalv.co.uk>
2018-02-18 10:36:43 -0800

    setup tmuxinator

--- zsh/base.zsh
+++ zsh/base.zsh
@@ -14,6 +14,6 @@ source ~/.dotfiles/zsh/history.zsh
 source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 # git clone git://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+# source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
 
 source ~/.dotfiles/bin/tmuxinator.zsh
